ef42f8e415631e2bbde876a66b1dea1b1a62482a
Julien Limoges <julien@limoges.co>
2015-05-31 14:39:24 -0400

    Added basic support for brokers
    Broker can now list current available topics, create a topic or add
    to an existing topic.

--- src/main/scala/co/limoges/hq/BrokerServlet.scala
+++ src/main/scala/co/limoges/hq/BrokerServlet.scala
@@ -1,17 +1,42 @@
 package co.limoges.hq
 
 import org.scalatra._
+import org.scalatra.Ok
+import scala.collection._
 import scalate.ScalateSupport
 
-class BrokerServlet extends HyperqueueStack {
+class BrokerServlet extends ScalatraServlet {
 
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
-  }
+	// Use our implementation of a broker to power the servlet
+	var broker = new Broker;
 
+	before() {
+		contentType = "application/json"
+	}
+	
+	get("/") {
+		broker.list()
+	}
+
+	post("/:topic") {
+		broker.add(params("topic"), params("event"))
+	}
 }
+
+case class Transaction(
+	topic:String,
+	event:String
+)
+
+class Broker {
+	var topics = mutable.Map[String, List[String]]().withDefaultValue(Nil)
+
+	def list() = topics.keys
+
+	def add(topic:String, event:String) : Transaction = {
+		topics(topic) ::= event
+		val tr = new Transaction(topic, event);
+		tr
+	}
+}
+
